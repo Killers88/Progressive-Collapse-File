fc70d5df35cec8c47f8d05c17853adcbd387187c
Killers88 <koleary@192.168.1.23>
2015-01-27 18:40:42 -0500

    Changed qo calculation to graph equations - more accurate

--- src/main/java/edu/mit/civil/blastassessment/calculation/PeakDynamicPressureQo.java
+++ src/main/java/edu/mit/civil/blastassessment/calculation/PeakDynamicPressureQo.java
@@ -7,7 +7,12 @@ package edu.mit.civil.blastassessment.calculation;
  */
 public class PeakDynamicPressureQo {
 
-	public static double CONSTANT_A = 0.022;
+	public static double CONSTANT_A = 0.0099;
+	public static double CONSTANT_B = 0.319;
+	public static double CONSTANT_C = 1.2759;
+	public static double CONSTANT_D = 7e-15;
+	public static double CONSTANT_E = 25;
+	public static double CONSTANT_F = 2400;
 
 	/**
 	 * Return Peak Dynamic Pressure 'qo' Could also use UFC 3-340 Fig 2.3 pg 155
@@ -18,7 +23,17 @@ public class PeakDynamicPressureQo {
 	 */
 	public static double calculatQoWithPso(final double pso) {
 
-		return CONSTANT_A * Math.pow(pso, 2);// psi;
+		if (pso < 100) {
+
+			return CONSTANT_A * Math.pow(pso, 2) + CONSTANT_B * pso
+					+ CONSTANT_C;// psi;
+
+		} else if ((pso > 100) && (pso < 130)) {
+
+			return CONSTANT_D * Math.pow(pso, 2) + CONSTANT_E * pso
+					- CONSTANT_F;// psi;
+		}
+		return 0;
 	}
 
 }
